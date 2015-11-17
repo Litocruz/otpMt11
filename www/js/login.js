e0591698eed56dafbed24210c42cbfd907f064dc
litocruz <rengolamadrid@hotmail.com>
2015-11-17 20:56:38 -0300

    mejoras en visualizacion

--- www/js/login.js
+++ www/js/login.js
@@ -17,6 +17,7 @@ if( $("#codigo").val() == "" )
 		$('#token').show();
 		$('#link').show();
 		$('#link').append($('<input>',{id:'secreto',value:respuestaServer.secreto, type:'hidden'}));
+		$('#link').append("<p>Escanear Qr para obtener token o ingresar secreto:  "+respuestaServer.secreto+ " en aplicacion Google Authenticator");
     $('#link').append($('<img>',{id:'qr',src:respuestaServer.qrCodeUrl}));
 		console.log("respuestaServer.validacion: "+respuestaServer.validacion+"\nsecreto: "+respuestaServer.secreto)
     alert("ingrese este valor en GA: "+respuestaServer.secreto)
